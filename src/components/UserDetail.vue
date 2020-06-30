af51eb1590dddf9ffa5dc70cd4b309ed76404c00
Mihir Sata <smihir@narola.local>
2020-06-30 17:42:43 +0530

    initial commit

---
+++ src/components/UserDetail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User name: {{ switchName() }} </p>
+        <p>User Age: {{ userAge }} </p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default{
+        props: {
+            name:{
+                type: String,
+                required: true
+
+            },
+            resetFn: Function,
+            userAge: Number
+            
+        },
+        methods:{
+            switchName(){
+                return this.name.split("").reverse().join("");
+            },
+            resetName(){
+                this.name="Max";
+                this.$emit('nameWasReset',this.name);
+            },
+           
+
+        }
+        
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
